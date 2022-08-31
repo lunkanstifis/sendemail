84752730d5d122f8c2a4000c407db270df5b9917
Stefan Lundmark <stefanlundmark@MacBook-som-tillhor-Stefan.local>
2022-08-31 17:45:37 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+email_sender = 'stelundmark@gmail.com'
+email_password = 'horwfsviubopkkyu'
+
+email_receiver = 'stelundmark@gmail.com'
+
+subject = "Don't forget email"
+
+body = """
+When you watch a video please subscribe
+"""
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['subject'] = subject
+em.set_content(body)
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
+
